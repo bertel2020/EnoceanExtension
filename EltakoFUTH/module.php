8b96228d8848888f610e70878c5581226c61a8ad
bertel2020 <72860283+bertel2020@users.noreply.github.com>
2024-01-01 15:17:07 +0100

    .

--- EltakoFUTH/module.php
+++ EltakoFUTH/module.php
@@ -108,10 +108,9 @@
 
     	    switch($data->DeviceID) {
     	        case $ID1:
-					$this->SetValue('Temperature', round(255-((int)$data->DataByte1))*(40/255));
-					$this->SetValue('SetTemp', round((int)$data->DataByte2)*(40/256));
+					$this->SetValue('Illumination', (int)$data->DataByte2 * (2048/255));
+					$this->SetValue('BatteryVoltage', (int)$data->DataByte3 * 0.02);
     	        case $ID2:
-					$this->SetValue('Humidity', ((int)$data->DataByte2)*(100/250>));
     	    	default:
     	            throw new Exception("Invalid Ident");
     	    }      
